e49bd3541d2f63d57a1863b780b5813fe12f9898
Luca Scopelliti BID <luca.scopelliti@bidcompany.it>
2026-01-13 14:45:47 +0100

    style: added types

--- chatbot-api/test/routers/test_health_router.py
+++ chatbot-api/test/routers/test_health_router.py
@@ -1,10 +1,12 @@
 import pytest
 
+from httpx import AsyncClient
+
 from src.routers.health import router as health_router
 
 
 @pytest.mark.asyncio
-async def test_health_check(client_test):
+async def test_health_check(client_test: AsyncClient):
     response = await client_test.get(url=health_router.prefix)
 
     assert response.status_code == 200
@@ -14,7 +16,7 @@ async def test_health_check(client_test):
     }
 
 @pytest.mark.asyncio
-async def test_health_check_db(client_test):
+async def test_health_check_db(client_test: AsyncClient):
     response = await client_test.get(url=f"{health_router.prefix}/db")
 
     assert response.status_code == 200
