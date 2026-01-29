ea58dc6fcb474af624b5a39b9b8e3fdd001a9c99
Luca Scopelliti BID <luca.scopelliti@bidcompany.it>
2026-01-29 10:33:43 +0100

    feat: chatbot-index worker uses queue adaptive-layer

--- chatbot-index/worker/src/worker/main.py
+++ chatbot-index/worker/src/worker/main.py
@@ -1,17 +1,25 @@
 import asyncio
 import logging
 
-from dos_utility.queue.redis import get_queue_client_ctx
+from dos_utility.queue import get_queue_client_ctx
+
+from .task import process_task
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 async def main():
     async with get_queue_client_ctx() as queue_client:
-        while True:
-            logging.info("Worker started and connected to the queue.")
-            logging.info("Waiting for tasks...")
+        logging.info("Worker started and connected to the queue.")
+        logging.info("Waiting for tasks...")
 
-            break #! Handle for tests, if needed remove it for production
+        while True:
+            msg, ack_token = await queue_client.dequeue()
+
+            if msg is not None:
+                await process_task(body=msg)
+
+                if ack_token is not None:
+                    await queue_client.acknowledge(ack_token=ack_token)
 
 
 if __name__ == "__main__":
--- chatbot-index/worker/test/main_test.py
+++ chatbot-index/worker/test/main_test.py
@@ -1,10 +1,21 @@
 import pytest
 
-from worker.main import main
+from src.worker import main
+
+from test.mocks import get_queue_client_ctx_mock, process_task_block_loop_mock, get_queue_client_loop_ctx_mock, StopAsyncIteration
 
 
 @pytest.mark.asyncio
-async def test_main():
-    await main()
+async def test_main_block_loop(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(main, "get_queue_client_ctx", get_queue_client_ctx_mock)
+    monkeypatch.setattr(main, "process_task", process_task_block_loop_mock)
 
-    assert True
+    with pytest.raises(expected_exception=Exception, match="Simulated processing error"):
+        await main.main()
+
+@pytest.mark.asyncio
+async def test_main_block_loop_at_second_iteration(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(main, "get_queue_client_ctx", get_queue_client_loop_ctx_mock)
+
+    with pytest.raises(expected_exception=StopAsyncIteration):
+        await main.main()
