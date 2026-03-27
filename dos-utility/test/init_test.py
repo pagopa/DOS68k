c9294cb150f73c933082204556079500ff742d74
Luca Scopelliti BID <luca.scopelliti@bidcompany.it>
2026-03-27 17:41:46 +0100

    test: dos-utility - removed main test

--- dos-utility/test/init_test.py
+++
@@ -1,8 +0,0 @@
-from dos_utility import main
-
-
-def test_main(capsys):
-    """Test that main() prints the expected greeting."""
-    main()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello from dos-utility!\n"
