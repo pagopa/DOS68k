40c752d14fc31af37b5935e620938c10521fdcdd
Luca Scopelliti BID <luca.scopelliti@bidcompany.it>
2026-04-10 11:26:19 +0200

    fix: dos-utility - optional aws credentials

--- dos-utility/src/dos_utility/utils/aws/env.py
+++ dos-utility/src/dos_utility/utils/aws/env.py
@@ -1,10 +1,11 @@
 from functools import lru_cache
+from typing import Annotated, Optional
 from pydantic_settings import BaseSettings
-from pydantic import Field, SecretStr
+from pydantic import SecretStr, Field
 
 class AWSCredentialsSettings(BaseSettings):
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: SecretStr
+    AWS_ACCESS_KEY_ID: Annotated[Optional[str], Field(default=None)]
+    AWS_SECRET_ACCESS_KEY: Annotated[Optional[SecretStr], Field(default=None)]
 
 @lru_cache
 def get_aws_credentials_settings() -> AWSCredentialsSettings:
