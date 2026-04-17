e0745a4a288978cf9662f4f13146390b3b8a60df
Luca Scopelliti BID <luca.scopelliti@bidcompany.it>
2026-04-17 11:35:04 +0200

    style: chatbot-index worker - ruff formatter

--- chatbot-index/worker/src/worker/embedder.py
+++ chatbot-index/worker/src/worker/embedder.py
@@ -6,16 +6,17 @@ from dos_utility.vector_db import ObjectData
 
 
 class Embedder:
-    def __init__(self,
-                 provider: str,
-                 embed_model_id: str,
-                 embed_batch_size: int,
-                 embed_dim: int,
-                 embed_task: str,
-                 embed_retries: int,
-                 embed_retry_min_seconds: int,
-                 model_api_key: str
-                 ):
+    def __init__(
+        self,
+        provider: str,
+        embed_model_id: str,
+        embed_batch_size: int,
+        embed_dim: int,
+        embed_task: str,
+        embed_retries: int,
+        embed_retry_min_seconds: int,
+        model_api_key: str,
+    ):
         self.provider = provider
         self.embed_model_id = embed_model_id
         self.embed_batch_size = embed_batch_size
@@ -35,36 +36,32 @@ class Embedder:
             retry_min_seconds=self.embed_retry_min_seconds,
             api_key=self.model_api_key,
         )
+
     def transform(self, chunks: list[ChunkData]) -> list[ObjectData]:
         embedded_chunks = []
         for chunk in chunks:
             embedding = self.embed_model.get_text_embedding(chunk.content)
-            embedded_chunks.append(
-                ObjectData(
-                    **chunk.model_dump(),
-                    vector = embedding
-                )
-            )
+            embedded_chunks.append(ObjectData(**chunk.model_dump(), vector=embedding))
         return embedded_chunks
-    
+
 
 def get_embedder(
     provider: str,
-    embed_model_id : str,
-    embed_batch_size : int,
-    embed_dim : int,
-    embed_task : str,
-    embed_retries : int,
+    embed_model_id: str,
+    embed_batch_size: int,
+    embed_dim: int,
+    embed_task: str,
+    embed_retries: int,
     embed_retry_min_seconds: int,
     model_api_key: str,
 ) -> Embedder:
     return Embedder(
-        provider = provider,
-        embed_model_id= embed_model_id,
-        embed_batch_size = embed_batch_size,
-        embed_dim = embed_dim,
-        embed_task = embed_task,
-        embed_retries = embed_retries,
-        embed_retry_min_seconds = embed_retry_min_seconds,
-        model_api_key=model_api_key
+        provider=provider,
+        embed_model_id=embed_model_id,
+        embed_batch_size=embed_batch_size,
+        embed_dim=embed_dim,
+        embed_task=embed_task,
+        embed_retries=embed_retries,
+        embed_retry_min_seconds=embed_retry_min_seconds,
+        model_api_key=model_api_key,
     )
--- chatbot-index/worker/src/worker/env.py
+++ chatbot-index/worker/src/worker/env.py
@@ -15,13 +15,16 @@ class TaskSettings(BaseSettings):
     vector_db_provider: str
 
     # Embedding settings
-    embed_model_id: Annotated[str, Field(default="ID of the embedding model for the chose provider")]
+    embed_model_id: Annotated[
+        str, Field(default="ID of the embedding model for the chose provider")
+    ]
     embed_batch_size: Annotated[PositiveInt, Field(default=100)]
     embed_dim: Annotated[PositiveInt, Field(default=768)]
     embed_task: Annotated[str, Field(default="RETRIEVAL_DOCUMENT")]
     embed_retries: Annotated[PositiveInt, Field(default=3)]
     embed_retry_min_seconds: Annotated[PositiveFloat, Field(default=1.0)]
     model_api_key: Annotated[str, Field(description="API key for the chosen provider")]
+
 
 @lru_cache()
 def get_task_settings() -> TaskSettings:
