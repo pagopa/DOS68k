8f14ff9e1ceb35758987d74c98817742c226ecd1
Luca Scopelliti BID <luca.scopelliti@bidcompany.it>
2026-04-17 11:29:46 +0200

    refactor: chatbot-index worker - vector db connection through context manager

--- chatbot-index/worker/src/worker/task.py
+++ chatbot-index/worker/src/worker/task.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 from typing import Any
 
@@ -7,7 +6,7 @@ from parsers import get_parser, ChunkData
 from embedder import get_embedder, ObjectData
 from env import get_task_settings
 
-from dos_utility.vector_db import get_vector_db_instance 
+from dos_utility.vector_db import get_vector_db_ctx
 
 task_settings = get_task_settings()
 loader = get_document_loader(bucket_name = task_settings.bucket_name)
@@ -22,7 +21,6 @@ embedder = get_embedder(
     embed_retry_min_seconds = task_settings.embed_retry_min_seconds,
     model_api_key = task_settings.model_api_key,
 )
-vector_db = get_vector_db_instance()
 
 async def process_task(body: bytes) -> None:
     """Process a task with the given body.
@@ -37,14 +35,12 @@ async def process_task(body: bytes) -> None:
     document : Document = loader.read(message=message)
     chunks: ChunkData = parser.transform(document= document)
     data_to_store: ObjectData = embedder.transform(chunks=chunks)
-    
-    indexes = await vector_db.get_indexes()
-    if message.indexId not in indexes:
-        await vector_db.create_index(
-            index_name = message.indexId,
-            vector_dim = task_settings.embed_dim
-                                     )
-    await vector_db.put_objects(data = data_to_store, index_name = message.indexId)
 
-
-    
+    async with get_vector_db_ctx() as vector_db:
+        indexes = await vector_db.get_indexes()
+        if message.indexId not in indexes:
+            await vector_db.create_index(
+                index_name = message.indexId,
+                vector_dim = task_settings.embed_dim
+            )
+        await vector_db.put_objects(data = data_to_store, index_name = message.indexId)
