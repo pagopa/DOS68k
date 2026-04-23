830d57c098dbdd235d5fe2f235861448a54ef208
Giorgio Barazzetta BID <giorgio.barazzetta@bidcompany.it>
2026-04-23 18:42:50 +0200

    [Add] Parsing strategy and update on already existing documents

--- chatbot-index/worker/src/worker/parsers.py
+++ chatbot-index/worker/src/worker/parsers.py
@@ -1,6 +1,8 @@
 from functools import lru_cache
 from typing import Annotated
 from pydantic import Field, BaseModel
+from llama_index.core.node_parser import SentenceSplitter 
+from llama_index.core import Document as RefDocument
 
 from loaders import Document
 
@@ -21,17 +23,35 @@ class ChunkData(BaseModel):
 
 
 class Parser:
+
+    def __init__ (self, chunk_size, chunk_overlap):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        print(f"Chunk size: {self.chunk_size} -- overlap: {self.chunk_overlap}")
+
+        self._splitter = SentenceSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap,
+        )
+
     def transform(self, document: Document) -> list[ChunkData]:
         filename = document.filename
-        chunk_content = ["\n".join(document.content)]
+        # chunk_content = ["\n".join(document.content)]
+        ref_document = RefDocument(text = "\n".join(document.content))
+        text_chunks = [x.text for x in self._splitter([ref_document])]
         chunks = []
-        for chunk_id, chunk in enumerate(chunk_content):
+        for chunk_id, text_chunk in enumerate(text_chunks):
             chunks.append(
-                ChunkData(filename=filename, chunk_id=chunk_id, content=chunk)
+                ChunkData(filename=filename, 
+                          chunk_id=chunk_id, 
+                          content=text_chunk)
             )
         return chunks
 
 
 @lru_cache
-def get_parser() -> Parser:
-    return Parser()
+def get_parser( chunk_size: int, chunk_overlap: int) -> Parser:
+    return Parser(
+        chunk_size = chunk_size,
+        chunk_overlap = chunk_overlap
+                  )
